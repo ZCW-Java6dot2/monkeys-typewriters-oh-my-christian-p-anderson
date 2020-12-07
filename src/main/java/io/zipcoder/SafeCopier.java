18c1a8084b8411a151bfa88fa963bcadade70ade
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-06 20:13:24 -0500

    added comments to SafeCopier to remind what locks are and how they are implemented

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -8,23 +8,30 @@ import java.util.concurrent.locks.*;
  */
 public class SafeCopier extends Copier {
 
-        private Lock lock = new ReentrantLock();
+    private Lock lock = new ReentrantLock();
 
 
     public SafeCopier(String toCopy) {
         super(toCopy);
     }
 
+    /**
+     * https://howtodoinjava.com/java/multi-threading/how-to-use-locks-in-java-java-util-concurrent-locks-lock-tutorial-and-example/
+     *
+     * First a Lock is created as a private field. Then we call this.lock and it's lock() method. Now the Lock instance
+     * is locked. Any other thread calling lock() will be blocked until the thread that locked the lock calls unlock().
+     * Finally unlock() is called, and the Lock is now unlocked so other threads can lock it.
+     */
     @Override
     public void run() {
-        lock.lock();
+        this.lock.lock();
 
         try {
             while(this.stringIterator.hasNext()) {
-                copied += stringIterator.next() + " ";
+                this.copied += stringIterator.next() + " ";
             }
         } finally {
-            lock.unlock();
+            this.lock.unlock();
         }
 
     }
